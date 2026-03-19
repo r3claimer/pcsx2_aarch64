56eedd9838cac171ed25ae20e93a76a5c0030e75
rocknix <macebrooks@gmail.com>
2026-03-19 21:59:56 +0000

    trying stuff

--- pcsx2/arm64/VixlHelpers.cpp
+++ pcsx2/arm64/VixlHelpers.cpp
@@ -1014,6 +1014,27 @@ void armPMOVMSKB(const a64::Register& regDst, const a64::VRegister& regSrc)
     armAsm->Mov(regDst, tmp.V8H(), 0);
 }
 
+static const struct {
+    int data[16][4];
+} shuffle = {
+    { 0, 1, 2, 3 },
+    { 1, 0, 3, 2 },
+    { 2, 3, 0, 1 },
+    { 3, 2, 1, 0 },
+    { 0, 2, 1, 3 },
+    { 1, 3, 0, 2 },
+    { 2, 0, 3, 1 },
+    { 3, 1, 2, 0 },
+    { 0, 3, 1, 2 },
+    { 1, 2, 0, 3 },
+    { 2, 1, 3, 0 },
+    { 3, 0, 2, 1 },
+    { 0, 0, 1, 1 },
+    { 1, 1, 2, 2 },
+    { 2, 2, 3, 3 },
+    { 3, 3, 0, 0 }
+};
+
 void armSHUFPS(const a64::VRegister& dstreg, const a64::VRegister& srcreg, int pIndex)
 {
     const auto* ptr = &shuffle.data[pIndex][1];
